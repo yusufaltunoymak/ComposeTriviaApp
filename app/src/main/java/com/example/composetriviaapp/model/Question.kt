42a707588cc9d8cbadfe1ff9b7e04e5b7675f802
altunoymak <yusuf_altunoymak@hotmail.com>
2023-11-02 16:51:06 +0300

    first commit

---
+++ app/src/main/java/com/example/composetriviaapp/model/Question.kt
@@ -0,0 +1,6 @@
+package com.example.composetriviaapp.model
+
+
+import com.google.gson.annotations.SerializedName
+
+class Question : ArrayList<QuestionItem>()
---
+++ app/src/main/java/com/example/composetriviaapp/model/QuestionItem.kt
@@ -0,0 +1,15 @@
+package com.example.composetriviaapp.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class QuestionItem(
+    @SerializedName("answer")
+    val answer: String?,
+    @SerializedName("category")
+    val category: String?,
+    @SerializedName("choices")
+    val choices: List<String?>?,
+    @SerializedName("question")
+    val question: String?
+)
